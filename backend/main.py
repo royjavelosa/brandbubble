0a2f08ef1f9d4b6fddafff7fac24dd3c8a496f95
Roy Javelosa <joseroyjavelosa@yahoo.com>
2026-03-23 15:21:55 -0700

    fix: add brandbubble-one.vercel.app to CORS whitelist

--- backend/main.py
+++ backend/main.py
@@ -16,6 +16,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://brandbubble.vercel.app"
+        "https://brandbubble-one.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
